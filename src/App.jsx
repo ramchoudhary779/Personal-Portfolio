c00c2e60345e276e0d0d88811dab2bede9fa0334
Ram Choudhary <ramchandrachoudhary960@gmail.com>
2025-06-25 00:28:07 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './pages/TopNav'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <div className="text-3xl">Hello</div>
+    </>
+  )
+}
+
+export default App
